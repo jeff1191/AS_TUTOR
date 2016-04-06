f796e8afcc63604f247bdef1e239aa7889070052
msalitu <mariasalgadoitu@gmail.com>
2016-04-06 12:09:21 +0200

    Vista de las tareas de un usuario sin posibilidad de edicion

--- app/src/main/java/es/ucm/as_tutor/presentacion/AdaptadorTareas.java
+++ app/src/main/java/es/ucm/as_tutor/presentacion/AdaptadorTareas.java
@@ -1,12 +1,12 @@
 package es.ucm.as_tutor.presentacion;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,6 +15,7 @@ import es.ucm.as_tutor.R;
 
 /**
  * Created by msalitu on 02/04/2016.
+ * Esta clase se corresponde con una fila de la tabla de tareas que tiene cada usuario
  */
 public class AdaptadorTareas extends BaseAdapter {
 
@@ -26,13 +27,13 @@ public class AdaptadorTareas extends BaseAdapter {
     ArrayList<Integer> si;
     ArrayList<Integer> no;
     ArrayList<String> frecuencias;
-    ArrayList<Boolean> habilitada;
+    ArrayList<Boolean> habilitadas;
     LayoutInflater inflater;
 
     public AdaptadorTareas(ArrayList<String> textosAlarma, ArrayList<String> horasAlarma,
                            ArrayList<String> textosPreguntas, ArrayList<String> horasPregunta,
                            ArrayList<Integer> si,ArrayList<Integer> no, ArrayList<String> frecuencias,
-                           ArrayList<Boolean> habilitada, Context context) {
+                           ArrayList<Boolean> habilitadas, Context context) {
         this.context = context;
         this.textosAlarma = textosAlarma;
         this.horasAlarma = horasAlarma;
@@ -41,7 +42,7 @@ public class AdaptadorTareas extends BaseAdapter {
         this.si = si;
         this.no = no;
         this.frecuencias = frecuencias;
-        this.habilitada = habilitada;
+        this.habilitadas = habilitadas;
     }
 
     @Override
@@ -61,24 +62,36 @@ public class AdaptadorTareas extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        TextView txtTitle;  // texto alarma
-        TextView sit;       // numero de si
-        TextView not;       // numero de no
-        TextView total;     // balance del progreso (si - no = total)
+        TextView txtTitle;      // texto alarma
+        TextView horaAlarma;    // hora alarma
+        TextView txtPregunta;   // texto pregunta
+        TextView horaPregunta;  // hora pregunta
+        TextView sit;           // numero de si
+        TextView not;           // numero de no
+        TextView total;         // balance del progreso (si - no = total)
+        TextView frecuencia;    // frecuencia
+        CheckBox habilitada;    // true si esta habilitada
 
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         View itemView = inflater.inflate(R.layout.tareas_row, parent, false);
 
         // Locacalizar en el listview_item.xml
-        // me falta encontrar todos los campos que no tiene t
         txtTitle = (TextView) itemView.findViewById(R.id.nombreTarea);
+        horaAlarma = (TextView) itemView.findViewById(R.id.horaAlarma);
+        txtPregunta = (TextView) itemView.findViewById(R.id.preguntaTarea);
+        horaPregunta = (TextView) itemView.findViewById(R.id.horaPregunta);
         sit = (TextView) itemView.findViewById(R.id.si);
         not = (TextView) itemView.findViewById(R.id.no);
         total = (TextView) itemView.findViewById(R.id.total);
+        frecuencia = (TextView) itemView.findViewById(R.id.frecuencia);
+        habilitada = (CheckBox) itemView.findViewById(R.id.habilitada);
 
-        // Dar valores a los TextView
+        // Dar valores a cada fila
         txtTitle.setText(textosAlarma.get(position));
+        horaAlarma.setText(horasAlarma.get(position));
+        txtPregunta.setText(textosPreguntas.get(position));
+        horaPregunta.setText(horasPregunta.get(position));
         String ss = si.get(position).toString();
         sit.setText(ss);
         String sn = no.get(position).toString();
@@ -89,6 +102,19 @@ public class AdaptadorTareas extends BaseAdapter {
         else
             total.setTextColor(Color.RED);
         total.setText(t.toString());
+        frecuencia.setText(frecuencias.get(position));
+        habilitada.setChecked(habilitadas.get(position));
+
+        // Poner el texto en color negro
+        txtTitle.setTextColor(Color.BLACK);
+        horaAlarma.setTextColor(Color.BLACK);
+        txtPregunta.setTextColor(Color.BLACK);
+        horaPregunta.setTextColor(Color.BLACK);
+        sit.setTextColor(Color.BLACK);
+        not.setTextColor(Color.BLACK);
+        total.setTextColor(Color.BLACK);
+        frecuencia.setTextColor(Color.BLACK);
+        habilitada.setTextColor(Color.BLACK);
 
         return itemView;
     }
