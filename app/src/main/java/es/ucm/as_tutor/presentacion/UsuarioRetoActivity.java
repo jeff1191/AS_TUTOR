659af1c1f53eb13563982e7b7b20f18c0fdf0d46
juanluar <jeanleon003@hotmail.com>
2016-04-07 01:20:29 +0200

    Activity reto falta para añadir uno nuevo

--- app/src/main/java/es/ucm/as_tutor/presentacion/UsuarioRetoActivity.java
+++ app/src/main/java/es/ucm/as_tutor/presentacion/UsuarioRetoActivity.java
@@ -1,7 +1,11 @@
 package es.ucm.as_tutor.presentacion;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import es.ucm.as_tutor.R;
 
@@ -10,9 +14,43 @@ import es.ucm.as_tutor.R;
  */
 public class UsuarioRetoActivity extends Activity{
 
+    private ProgressBar progreso;
+    private Integer contInt;    // contador del reto
+    private TextView c;         // muestra el contador
+    private TextView tv;        // muestra el texto del reto
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario_reto);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null){
+            String nombre = bundle.getString("textReto");
+
+            tv = (TextView) findViewById(R.id.textoReto);
+            tv.setText(nombre);
+
+            contInt = bundle.getInt("contadorReto");
+            c = (TextView) findViewById(R.id.cont);
+            c.setText(contInt.toString()+"/30");
+            progreso = (ProgressBar) findViewById(R.id.progressBar);
+            progreso.setProgress(contInt);
+
+            boolean superado = bundle.getBoolean("superadoReto");
+
+            if (superado) {
+                TextView sup = (TextView) findViewById(R.id.reto);
+                sup.setText("RETO SUPERADO");
+            }else {
+                progreso = (ProgressBar) findViewById(R.id.progressBar);
+                progreso.setVisibility(View.INVISIBLE);
+                TextView sup = (TextView) findViewById(R.id.reto);
+                sup.setText("No tiene ningún reto asignado");
+                sup.setTextColor(Color.GRAY);
+            }
+
+        }
     }
 }
