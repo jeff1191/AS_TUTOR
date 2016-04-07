ac751c4219fed199240515ef3ca435651954025f
msalitu <mariasalgadoitu@gmail.com>
2016-04-07 11:25:07 +0200

    Orientacion obligada

--- app/src/main/java/es/ucm/as_tutor/presentacion/UsuarioTareaDetalleActivity.java
+++ app/src/main/java/es/ucm/as_tutor/presentacion/UsuarioTareaDetalleActivity.java
@@ -32,7 +32,6 @@ public class UsuarioTareaDetalleActivity extends AppCompatActivity {
     private Spinner frecuenciaSpinner;
     private TextView total;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
