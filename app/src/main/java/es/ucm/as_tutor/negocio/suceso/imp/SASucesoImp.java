acc51142b5d7678700c0ad4b53ad9dd71f99e39a
msalitu <mariasalgadoitu@gmail.com>
2016-04-15 12:16:25 +0200

    Dao

--- app/src/main/java/es/ucm/as_tutor/negocio/suceso/imp/SASucesoImp.java
+++ app/src/main/java/es/ucm/as_tutor/negocio/suceso/imp/SASucesoImp.java
@@ -2,9 +2,13 @@
 package es.ucm.as_tutor.negocio.suceso.imp;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
 
 import es.ucm.as_tutor.integracion.DBHelper;
 import es.ucm.as_tutor.negocio.suceso.SASuceso;
+import es.ucm.as_tutor.negocio.suceso.Tarea;
 import es.ucm.as_tutor.presentacion.vista.main.Manager;
 
 
@@ -21,16 +25,29 @@ public class SASucesoImp implements SASuceso {
 
 	@Override
 	public void crearTarea() {
+		try {
+			Dao<Tarea, Integer> daoTarea = mDBHelper.getTareaDao();
 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void editarTarea() {
-
+		try {
+			Dao<Tarea, Integer> daoTarea = mDBHelper.getTareaDao();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void eliminarTarea() {
-
+		try {
+			Dao<Tarea, Integer> daoTarea = mDBHelper.getTareaDao();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
